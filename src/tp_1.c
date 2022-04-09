59413572742fa2eb28aaa023ac49033341666ba0
Brunocasta <castabruno92@gmail.com>
2022-04-09 01:00:03 -0300

    cambios

--- src/tp_1.c
+++ src/tp_1.c
@@ -10,37 +10,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int PedirEntero(char mensaje[]);
 int ValidarLimite(int numero,int limiteInferior);
 void Descuento(int a,float descuento);
 void Interes(int b,float interes);
 void BitcoinValor(int c);
+void PrecioUnitario ( int numero,float precioUnitario);
 
 int main() {
 	setbuf(stdout,NULL);
 	int x,y,z;
 
-	printf("Ingresar Kilometros :");
+	//kilometros
+	x=PedirEntero("Ingresar Kilometros :");
+	//printf("\nKms Ingresados: %d kms",x);
+	//ValidarLimite(x,-1);
+	/*printf("Ingresar Kilometros :");
 	scanf("%d",&x);
-	x=ValidarLimite(x,0);
-
-	printf("Precio vuelo Aerolineas: ");
+	x=ValidarLimite(x,1);
+	/*printf("Precio vuelo Aerolineas: ");
 	scanf("%d",&y);
-
 	printf("Precio vuelo Latam: ");
-	scanf("%d",&z);
+	scanf("%d",&z);*/
+	y=PedirEntero("Precio vuelo Aerolineas :");
+
+	z=PedirEntero("Precio vuelo Latam: ");
+
+
+
 
 	//DESCUENTO AEROLINEAS
 	Descuento(y,0.9);
 	//DESCUENTO LATAM
 	Descuento(z,0.9);
+
 	//INTERES AEROLINEAS
 	Interes(y,1.25);
-	//INTERES AEROLINEAS
+	//INTERES LATAM
 	Interes(z,1.25);
 
+	//	BIT AEROLINEAS
 	BitcoinValor(y);
+	//BIT LATAM
+	BitcoinValor(z);
+
+	//UNITARIO AEROLINEAS
+	PrecioUnitario(y,x);
+	//UNITARIO LATAM
+	PrecioUnitario(z,x);
 
 	return 0;
+}
+int PedirEntero(char mensaje[])//establezco para mostrar un mensaje,y los limite
+{
+    int numero;
+
+    printf("%s", mensaje);
+    scanf("%d",&numero);
+    numero=ValidarLimite(numero,1);
+
+    return numero;
 }
 
 int ValidarLimite(int numero,int limiteInferior)
@@ -76,6 +105,12 @@ void BitcoinValor(int c)
 	printf("\nEl valor en Bitcoin es :%f BTN",totalBitcoin);
 
 }
+void PrecioUnitario ( int numero,float precioUnitario)
+{
+	float totalUnitario;
+	totalUnitario =numero /precioUnitario;
+	printf("\nEl precio unitario del vuelo es:%f",totalUnitario);
 
 
+}
 
